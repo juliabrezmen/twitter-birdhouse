8d45b2aa3103f63ee7c2521f2e91a5d7f2458ef4
Julia <juliyabrezmen@gmail.com>
2015-12-24 22:24:47 +0200

    Initial commit

---
+++ app/src/main/java/com/bd/BirdhouseApplication.java
@@ -0,0 +1,20 @@
+package com.bd;
+
+import android.app.Application;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+public class BirdhouseApplication extends Application {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "IkgMPY7xbqEN2D86EDDadiBbM";
+    private static final String TWITTER_SECRET = "cM6rrMTxMYkGKndlOd1E0hjrbxOfOzZw9TbJN9slfVOt7YNjFd";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
